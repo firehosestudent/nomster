ce60998b08f68fbeed818c1bf5ada5588cfbab1d
firehosestudent <johnhuskyplastics@gmail.com>
2017-06-06 01:45:58 +0000

    photo create

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :places do
     resources :comments, only: :create
-    resources :pictures
+    resources :photos
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
