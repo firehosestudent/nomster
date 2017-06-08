677266eac2d7b89fb3d4e6c06c7e1cc5b9ef25b5
firehosestudent <johnhuskyplastics@gmail.com>
2017-06-08 03:03:06 +0000

    mailer

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,13 @@
+class NotificationMailer < ApplicationMailer
+
+  default from: "no-reply@nomsterapp.com"
+
+  def comment_added
+    mail(to: "jonny_phun@hotmail.com",
+      subject: "A comment has been added to your place")
+  end
+
+end
+
+
+
