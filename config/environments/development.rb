b722cbb7e202ff461623094e99449692bd93f759
firehosestudent <johnhuskyplastics@gmail.com>
2017-06-08 22:27:02 +0000

    mailer edit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,8 @@
 Rails.application.configure do
+
+
+  
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -45,6 +49,19 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+
+
 
   config.action_mailer.default_url_options = { host: 'localhost:3030'}
 
@@ -55,3 +72,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+
+
